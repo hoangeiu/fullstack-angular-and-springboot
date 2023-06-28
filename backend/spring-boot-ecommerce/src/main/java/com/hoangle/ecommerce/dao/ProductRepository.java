36c58e23987e34b0e2bbd32da76094af83a0f119
Lê Minh Hoàng <leminhhoangk3@gmail.com>
2023-06-28 22:25:08 +0700

    implement routes and add api findByCategoryId

--- backend/spring-boot-ecommerce/src/main/java/com/hoangle/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/hoangle/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.hoangle.ecommerce.dao;
 
 import com.hoangle.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // Spring Data Rest automatically exposes endpoint http://localhost:8080/api/products/search/findByCategoryId?id=2
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
